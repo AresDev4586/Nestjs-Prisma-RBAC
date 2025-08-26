0775c1ccbffac2a4109f5db0f92b5e8d9d0e3c44
Ronnye <137380734+duraznoplay@users.noreply.github.com>
2025-08-25 20:32:43 -0500

    Deploy to vercel

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,17 +1,17 @@
-import { Controller,Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UsersService } from '@users/services/users.service';
 
 @Controller('users')
 export class UsersController {
-   constructor(private usersService: UsersService) {}
+    constructor(private usersService: UsersService) { }
 
-  @Post()
-  create(@Body() body: { email: string; name?: string }) {
-    return this.usersService.createUser(body.email, body.name);
-  }
+    @Post()
+    create(@Body() body: { email: string; name?: string }) {
+        return this.usersService.createUser(body.email, body.name);
+    }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAllUsers();
-  }
+    @Get()
+    findAll() {
+        return this.usersService.findAllUsers();
+    }
 }
--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -2,15 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../prisma/services/prisma.service';
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) { }
 
-  createUser(email: string, name?: string) {
-    return this.prisma.user.create({
-      data: { email, name },
-    });
-  }
+    createUser(email: string, name?: string) {
+        return this.prisma.user.create({
+            data: { email, name },
+        });
+    }
 
-  findAllUsers() {
-    return this.prisma.user.findMany();
-  }
+    findAllUsers() {
+        return this.prisma.user.findMany();
+    }
 }
